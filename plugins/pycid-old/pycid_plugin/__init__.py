5906fbda1ef95a1bd221a73b05a8ba370f00ec3a
Elazar Gershuni <elazarg@gmail.com>
2026-02-01 22:45:46 +0200

    remove pycid-old

--- plugins/pycid-old/pycid_plugin/__init__.py
+++
@@ -1,8 +0,0 @@
-"""PyCID plugin service - MAID Nash equilibrium analysis."""
-
-# Compatibility shim for pgmpy 0.1.17 which uses deprecated np.product
-# (removed in numpy 2.0). Must be applied before importing pgmpy.
-import numpy as np
-
-if not hasattr(np, "product"):
-    np.product = np.prod
